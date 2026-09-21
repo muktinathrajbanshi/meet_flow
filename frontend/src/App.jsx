fda99ea5ad84d8b808bba18afddbfbd5b8997e75
Muktinath Rajbanshi <muktinathrajbanshi786@gmail.com>
2026-09-21 13:10:26 +0545

    wow perfect

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,34 @@
 import { Toaster } from "react-hot-toast";
-import { Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
+import Login from "./pages/Login";
+import ProtectedRoute from "./components/ProtectedRoute";
+import ProtectedLayout from "./components/ProtectedLayout";
+import Dashboard from "./pages/Dashboard";
+import Session from "./pages/Session";
+import Pricing from "./pages/Pricing";
+import MeetingRoom from "./pages/MeetingRoom";
 
 const App = () => {
   return (
     <>
       <Toaster />
-      <Routes></Routes>
+      <Routes>
+        {/* Public Routes  */}
+        <Route path="/login" element={<Login mode="login" />} />
+        <Route path="/register" element={<Login mode="register" />} />
+
+        {/* Private Routes  */}
+        <Route element={<ProtectedRoute />}>
+          <Route element={<ProtectedLayout />}>
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/sessions" element={<Session />} />
+            <Route path="/pricing" element={<Pricing />} />
+          </Route>
+          <Route path="/meeting/:meetingId" element={<MeetingRoom />} />
+        </Route>
+
+        {/* other Routes  */}
+      </Routes>
     </>
   );
 };
