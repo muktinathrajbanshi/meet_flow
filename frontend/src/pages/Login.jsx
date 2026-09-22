ea25d4066fe9583bbd05bfa4a7224ed637ffb4f8
Muktinath Rajbanshi <muktinathrajbanshi786@gmail.com>
2026-09-22 13:30:20 +0545

    toh chalo abhi rakhte hain

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,5 +1,14 @@
-const Login = () => {
-  return <div>Login</div>;
+const Login = ({ mode = "login" }) => {
+  return (
+    <div
+      className="min-h-screen w-full bg-[url('/login_bg.png')] text-slate-800 p-4 md:p-6 lg:p-8
+    flex items-center justify-center font-sans"
+    >
+      <div className="w-full flex justify-center py-2">
+        <p>{mode}</p>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
