f0f37600a53e003c944ab93d3f75ce201d3d3c68
Muktinath Rajbanshi <muktinathrajbanshi786@gmail.com>
2026-09-25 21:19:24 +0545

    wow vidu gopal aiya

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,5 +1,16 @@
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div>
+      <footer
+        className="w-full max-w-305 mx-auto bg-white/10 backdrop-blur xl:rounded-t-xl
+      px-6 py-4 text-center"
+      >
+        <p className="text-xs font-medium text-white">
+          © 2026 MeetFlow. All rights reserved.
+        </p>
+      </footer>
+    </div>
+  );
 };
 
 export default Footer;
